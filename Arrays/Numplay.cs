6528bbf4462b83137aba965c8c79f3b294491eb5
SULFIKKAR E S <sulfikkeres369@hotmail.com>
2022-01-07 23:37:12 +0530

    https://github.com/SulfikkarES

--- Arrays/Numplay.cs
+++ Arrays/Numplay.cs
@@ -1,8 +1,28 @@
-﻿using System;
+﻿#region Copyright EazyLearn
+//
+// All rights are reserved. Reproduction or transmission in whole or in part, 
+// in any form or by any means, electronic, mechanical or otherwise, is
+// prohibited without the prior written consent of the copyright owner.
+//
+// Filename        : Person.cs
+// Purpose         : To practice C# Array
+// Creation Date   : 24-09-2021
+// Author          : Sulfikkar E S
+//
+// Change History  :
+// Changed by      :              
+// Date            : 
+// Purpose         :
+//
+#endregion Copyright EazyLearn
+
+#region Included namespaces
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+#endregion 
 
 namespace RoughSpace
 {
